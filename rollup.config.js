7c7a2825ec4201670d449992e8660aca38d4aac4
geraldalewis <gerald.a.lewis@gmail.com>
2017-06-07 14:05:46 -0400

    module support; extracted png-to-jpng

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import uglify from 'rollup-plugin-uglify';
 
+let pkg = require('./package.json');
+
 export default {
   entry: 'lib/jpng.js',
+  dest: pkg.main,
   format: 'umd',
-  dest: 'dist/jpng.js',
-  plugins: [
-    uglify()
-  ]
+  moduleName: 'jpng',
+  plugins: [uglify()]
 };
